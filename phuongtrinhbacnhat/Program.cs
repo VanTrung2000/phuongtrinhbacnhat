ff3009e0798b32693beee89035609d6326a952bc
VanTrung2000 <vantrungdeptrai2000@gmail.com>
2020-09-08 22:01:34 +0700

    Add project files.

---
+++ phuongtrinhbacnhat/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace phuongtrinhbacnhat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("phuong trinh bac nhat dang: 'a * x + b = 0'");
+
+            Console.Write("a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            if (a != 0)
+            {
+                double solution = -b / a;
+                Console.Write("phuong trinh co nghiem duy nhat: {0}", solution);
+            }
+            else
+            {
+                if (b == 0)
+                {
+                    Console.Write("Phuong trinh vo so nghiem");
+                }
+                else
+                {
+                    Console.Write("Phuong trinh vo nghiem");
+                  
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
